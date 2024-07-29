fa42015b2310cec648372762470200e67f287c36
wolfsoul01 <alejandrof011021@gmail.com>
2024-07-29 16:13:17 -0400

    ....

--- src/components/Orbital.tsx
+++ src/components/Orbital.tsx
@@ -11,7 +11,7 @@ export function OrbitingCirclesDemo({ children }: Props) {
 
   
   return (
-    <div className="relative flex h-[80vh] w-full flex-col items-center justify-center  rounded-lg  bg-background ">
+    <div className="relative flex h-[80vh] w-full flex-col items-center justify-center  rounded-lg ">
       <span className="pointer-events-none whitespace-pre-wrap flex justify-center items-center pb-14 pr-4 ">
         {children}
       </span>
