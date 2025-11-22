8c67a011ba8a7424615d0a276ba5a48f63906392
astro <alejandrodev01@gmail.com>
2025-11-21 22:40:03 -0500

    feat: actualizar descripciones y mejorar la estructura de proyectos en la sección de habilidades

--- src/components/Orbital.tsx
+++ src/components/Orbital.tsx
@@ -93,7 +93,7 @@ export function OrbitingCirclesDemo({ children }: Props) {
         className="size-[50px] border-none bg-transparent"
         radius={maxRadius}
         duration={20}
-        delay={4}
+        delay={3}
         reverse
       >
         <Icons.nextJs />
@@ -103,7 +103,7 @@ export function OrbitingCirclesDemo({ children }: Props) {
         className="size-[50px] border-none bg-transparent"
         radius={maxRadius}
         duration={20}
-        delay={8}
+        delay={6}
         reverse
       >
         <Icons.astro />
@@ -113,7 +113,7 @@ export function OrbitingCirclesDemo({ children }: Props) {
         className="size-[50px] border-none bg-transparent"
         radius={maxRadius}
         duration={20}
-        delay={12}
+        delay={20}
         reverse
       >
         <Icons.angular />
@@ -123,10 +123,20 @@ export function OrbitingCirclesDemo({ children }: Props) {
         className="size-[50px] border-none bg-transparent"
         radius={maxRadius}
         duration={20}
-        delay={16}
+        delay={12}
         reverse
       >
         <Icons.tailwind />
+      </OrbitingCircles>
+
+      <OrbitingCircles
+        className="size-[50px] border-none bg-transparent"
+        radius={maxRadius}
+        duration={20}
+        delay={15}
+        reverse
+      >
+        <Icons.expo />
       </OrbitingCircles>
     </div>
   );
@@ -367,6 +377,17 @@ const Icons = {
     >
       <rect width="512" height="512" rx="15%" fill="#f7df1e" />
       <path d="M324 370c10 17 24 29 47 29c20 0 33-10 33 -24c0-16 -13 -22 -35 -32l-12-5c-35-15 -58 -33 -58 -72c0-36 27 -64 70 -64c31 0 53 11 68 39l-37 24c-8-15 -17 -21 -31 -21c-14 0-23 9 -23 21c0 14 9 20 30 29l12 5c41 18 64 35 64 76c0 43-34 67 -80 67c-45 0-74 -21 -88 -49zm-170 4c8 13 14 25 31 25c16 0 26-6 26 -30V203h48v164c0 50-29 72 -72 72c-39 0-61 -20 -72 -44z" />
+    </svg>
+  ),
+  expo: () => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="#000000"
+      width="800px"
+      height="800px"
+      viewBox="0 0 32 32"
+    >
+      <path d="M24.292 15.547c1.968 0.131 3.729-1.213 4.115-3.145-0.475-0.735-1.287-1.177-2.161-1.177-2.272-0.052-3.491 2.651-1.953 4.323zM15.115 4.697l5.359-3.104-1.708-0.963-7.391 4.281 0.589 0.328 1.119 0.629 2.032-1.176zM21.161 1.307c0.089 0.027 0.161 0.1 0.188 0.188l2.484 7.593c0.047 0.131-0.005 0.272-0.125 0.344-1.968 1.156-2.916 3.489-2.317 5.693 0.656 2.391 2.937 3.953 5.401 3.703 0.135-0.011 0.265 0.073 0.307 0.203l2.563 7.803c0.041 0.131-0.011 0.271-0.125 0.344l-7.859 4.771c-0.037 0.021-0.084 0.036-0.131 0.036-0.068 0.016-0.14 0-0.203-0.041l-2.765-1.797c-0.048-0.031-0.084-0.077-0.109-0.129l-5.396-12.896-8.219 4.875c-0.016 0.011-0.037 0.021-0.052 0.032-0.084 0.036-0.183 0.025-0.261-0.021l-1.859-1.093c-0.136-0.073-0.188-0.245-0.115-0.381l7.953-15.749c0.025-0.057 0.077-0.104 0.135-0.131l7.959-4.609c0.088-0.052 0.197-0.057 0.292-0.005zM12.839 6.407l-1.932-1.089-7.693 15.229 1.396 0.823 6.631-9.015c0.063-0.089 0.167-0.136 0.271-0.12 0.104 0.011 0.192 0.077 0.235 0.177l7.228 17.296 1.933 1.251-8.063-24.552zM26.245 16.964c-2.256 0-3.787-2.292-2.923-4.376 0.86-2.083 3.563-2.619 5.156-1.025 0.595 0.593 0.928 1.396 0.928 2.235 0.005 1.749-1.412 3.167-3.161 3.167z" />
     </svg>
   ),
 };
